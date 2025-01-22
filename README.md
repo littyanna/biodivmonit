5a85c8842cdec6159efd0faefbb9a2aaf6f41da1
statdoctor <littymathew3008@gmail.com>
2025-01-22 16:04:41 +0000

    new readme files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # biodivmonit
+
 The data and code for analysing biodiversity monitoring data using Hidden Markov Models.
+
+Repository for data and code to reproduce Hidden Markov model analyses for this work.
+
+Data folder includes .rda data for bird species detections and final data for the analysis.  Data structure is described in the corresponding README file for the data..
+
+R folder contains R script for aggregating the bird species detection data, preparing the final data for analysis bird species detections, and analysing the data using HMMs. 
---
+++ Raw data/aggregate.R
@@ -0,0 +1,16 @@
+rm(list=ls())
+library(dplyr)
+library(ggplot2)
+
+
+
+load("C:/Users/litty/OneDrive - Trinity College Dublin/TCD_PhD/Caroline weekily meeting/2024/12. December/OKEON paper_final r codes_01122024/biodivmonit/Raw data/2022_Species_detections.rda")
+data<-Species_detections %>% select(Date,Period, Vocalisations)
+data$Date<-as.Date(data$Date,format = "%d-%m-%Y")
+data<-data%>%arrange(data$Date)
+overall_n<-data%>%group_by(Date,Period)%>% summarise(Vocalisations= sum(Vocalisations))%>% ungroup() # Removes the grouping
+overall_n$voc<-c(NA,(log(overall_n$Vocalisations[-1])-log(overall_n$Vocalisations[-length(overall_n$Vocalisations)])))
+overall<-na.omit(overall_n)
+str(overall)
+
+#write.csv(overall, "D:/Litty-One drive TCD/OneDrive - Trinity College Dublin/TCD_PhD/Caroline weekily meeting/2024/9. September/To silvia - HMM materials/OKEON old paper R pgms latest/Updated r codes_okeon old_140924/okeon_old.csv")
