e9d27c13f90bff32c5f0384ae7d56d3414302812
Litty Mathew <littymathew3008@gmail.com>
2025-04-22 07:25:09 +0100

    comments

--- Raw data/aggregate.R
+++ Raw data/aggregate.R
@@ -1,4 +1,4 @@
-#This is the code used to aggregate the data across species and site in a daily resolution. 
+#This is the R code used to aggregate the raw data (2022_species_detections.rda) across species and sites on a daily basis.
 rm(list=ls())
 library(dplyr)
 library(ggplot2)
@@ -7,7 +7,7 @@ load("D:/Litty-One drive TCD/OneDrive - Trinity College Dublin/TCD_PhD/OKEON_HMM
 data<-Species_detections %>% select(Date,Period, Vocalisations)
 data$Date<-as.Date(data$Date,format = "%d-%m-%Y")
 data<-data%>%arrange(data$Date)
-overall_n<-data%>%group_by(Date,Period)%>% summarise(Vocalisations= sum(Vocalisations))%>% ungroup() # Removes the grouping
+overall_n<-data%>%group_by(Date,Period)%>% summarise(Vocalisations= sum(Vocalisations))%>% ungroup() 
 overall_n$Voc<-c(NA,(log(overall_n$Vocalisations[-1])-log(overall_n$Vocalisations[-length(overall_n$Vocalisations)])))
 str(overall_n)
 
